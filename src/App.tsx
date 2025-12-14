76117234da70302853ad4b230cc05e74ee012990
Liam Andersson <liam.andersson2002@gmail.com>
2025-12-14 14:34:22 +0100

    Added README

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
+import Footer from './components/Footer.tsx'
 import Header from './components/Header.tsx'
+import Separator from './components/Separator.tsx'
 import { ThemeProvider } from './providers/ThemeProvider.tsx'
 
 // Sections for this page (app)
 import Skills from './sections/app/Skills.tsx'
 import Home from './sections/app/Home.tsx'
-import Separator from './components/Separator.tsx'
 import Projects from './sections/app/Projects.tsx'
-import Footer from './components/Footer.tsx'
 
 const App = () => {
   return (
